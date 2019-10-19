52f0d384f5a953af6bc6f4d9d9c64b0553e4a9f6
Kontain <oakanawi@gmail.com>
2019-10-19 11:18:05 -0700

    Finished adding censor functionality for recurring negative words.

--- censor_dispenser/censor_dispenser.py
+++ censor_dispenser/censor_dispenser.py
@@ -20,8 +20,9 @@ def censorFromText(text):
 censorFromText(email_one)"""
 
 censored = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-def censorFromText(text):
+"""def censorFromText(text):
 	#text_lowercase = text.lower()
 	#print(text_lowercase)
 	text_split = text.split()
@@ -45,12 +46,56 @@ def censorFromText(text):
 							#print(i)
 							#print(text_split[word + i])
 						text_split[word + i] = "*" * len(text_split[word + i])
-	#print(text_split)
-		#for word in range(len(text_split)):
-			#if phrase == text_split[word].lower():
-				#print(text_split[word])
-				#text_split[word] = "*" * len(text_split[word])
-				#print(word)
+
 	print(" ".join(text_split))
 
-censorFromText(email_two)
+#censorFromText(email_two) """
+
+def censorFromText(text):
+	text_split = text.split()
+	neg_stored = []
+
+	for word in text_split:
+		#print(word)
+		for letter in word:
+			if letter.lower() not in "abcdefghijklmnopqrstuvwxyz":
+				word = word.replace(letter, "")
+			#print(word)
+
+	for word in range(len(text_split)):
+		for neg in negative_words:
+			if neg == text_split[word].lower():
+				#print(text_split[word])
+				neg_stored.append(text_split[word])
+				if neg != neg_stored[0]:
+					#print(text_split[word])
+					text_split[word] = "*" * len(text_split[word])
+					#print(text_split[word])
+	#print(neg_stored)
+	#print(len(neg_stored))
+	#print(range(len(neg_stored)))
+
+	for phrase in censored:
+		#Checking for censored phrases that are not a single word
+		#if " " in phrase:
+		phrase_split = phrase.split()
+			#print(phrase_split)
+		for word in range(len(text_split)):
+				#print(word)
+			if text_split[word].lower() == phrase_split[0]:
+				match_count = []
+				for p in range(len(phrase_split)):
+					if phrase_split[p] in text_split[word + p].lower():
+						match_count.append(text_split[word + p])
+					#print(match_count)
+				if range(len(match_count)) == range(len(phrase_split)):
+						#print(match_count)
+						#print(text_split[word].replace(text_split[word], "*" * len(text_split[word])))
+					for i in range(len(match_count)):
+							#print(i)
+							#print(text_split[word + i])
+						text_split[word + i] = "*" * len(text_split[word + i])
+
+	print(" ".join(text_split))
+
+censorFromText(email_three)
